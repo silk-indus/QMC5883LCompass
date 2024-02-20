c5cc2547b8f083a38f6646552ce10ccd5cf24b78
silk-indus <67788780+silk-indus@users.noreply.github.com>
2024-02-20 14:43:37 +0100

    Update QMC5883LCompass.h
    Added calibration to the Earth magnetic field.

--- src/QMC5883LCompass.h
+++ src/QMC5883LCompass.h
@@ -14,8 +14,8 @@ class QMC5883LCompass{
     void setMode(byte mode, byte odr, byte rng, byte osr);
 	void setMagneticDeclination(int degrees, uint8_t minutes);
 	void setSmoothing(byte steps, bool adv);
-	void calibrate();
-	void setCalibration(int x_min, int x_max, int y_min, int y_max, int z_min, int z_max);
+	void calibrate(float norm_flux);
+	void setCalibration(float norm_flux, int x_min, int x_max, int y_min, int y_max, int z_min, int z_max);
 	void setCalibrationOffsets(float x_offset, float y_offset, float z_offset);
 	void setCalibrationScales(float x_scale, float y_scale, float z_scale);
     float getCalibrationOffset(uint8_t index);
